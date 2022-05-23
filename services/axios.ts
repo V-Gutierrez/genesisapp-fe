37b6789ec71e84107d47b0bdb6e456679797d554
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2022-05-23 18:28:51 -0300

    fix shit

--- services/axios.ts
+++ services/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const Axios = axios.create({
   withCredentials: true,
-  baseURL: process.env.NEXT_PUBLIC_API,
+  baseURL: process.env.NEXT_PUBLIC_API_BASEURL,
 });
 
 export default Axios;
