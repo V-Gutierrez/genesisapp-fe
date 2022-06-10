1df5e6d370994b467d10df1f4f54cbeb2b0128a7
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2022-06-10 14:42:10 -0300

    chore: remove arrows

--- sections/Home/components/DevotionalsSlider.tsx
+++ sections/Home/components/DevotionalsSlider.tsx
@@ -1,27 +1,27 @@
 import {
  Box, Flex, Text, useBreakpointValue,
-} from '@chakra-ui/react';
+} from '@chakra-ui/react'
 
-import CustomSlider from 'components/CustomSlider';
-import DevotionalCard from 'components/DevotionalCard';
-import { GET_USER_DEVOTIONALS } from 'services/queries';
-import { inHours } from 'helpers/time';
-import { useQuery } from 'react-query';
+import CustomSlider from 'components/CustomSlider'
+import DevotionalCard from 'components/DevotionalCard'
+import { GET_USER_DEVOTIONALS } from 'services/queries'
+import { inHours } from 'helpers/time'
+import { useQuery } from 'react-query'
 
 const DevotionalsSlider: React.FC = () => {
   const { data } = useQuery('devotionals', GET_USER_DEVOTIONALS, {
     staleTime: inHours(24),
     cacheTime: inHours(24),
-  });
+  })
   const slides = useBreakpointValue({
     base: 1,
     md: 2,
     lg: 3,
     xl: 4,
     '2xl': 6,
-  });
+  })
 
-  if (!data || !data.data.length) return null;
+  if (!data || !data.data.length) return null
   return (
     <Box w="100%" p={{ md: 6 }}>
       <Text
@@ -39,6 +39,7 @@ const DevotionalsSlider: React.FC = () => {
             centerMode: true,
             centerPadding: '5px',
             autoplay: false,
+            arrows: false,
           }}
         >
           {data?.data.map((devotional) => (
@@ -47,7 +48,7 @@ const DevotionalsSlider: React.FC = () => {
         </CustomSlider>
       </Flex>
     </Box>
-  );
-};
+  )
+}
 
-export default DevotionalsSlider;
+export default DevotionalsSlider
