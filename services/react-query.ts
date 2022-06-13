dac2ba322ffaaf03ce03142929dbb57c24800276
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2022-06-13 12:24:49 -0300

    chore: add react query prop

--- services/react-query.ts
+++ services/react-query.ts
@@ -1,14 +1,14 @@
-import { QueryClient } from 'react-query';
+import { QueryClient } from 'react-query'
 
 const ReactQueryClient = new QueryClient({
   defaultOptions: {
     queries: {
       refetchOnMount: false,
-      refetchOnWindowFocus: false,
+      refetchOnWindowFocus: true,
       keepPreviousData: true,
       retryOnMount: false,
     },
   },
-});
+})
 
-export default ReactQueryClient;
+export default ReactQueryClient
