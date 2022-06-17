0ce1b1a59a027afc12eb590dbf614db7f2bc1359
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2022-06-17 16:21:10 -0300

    fix: test imports

--- components/Leaflet/index.tsx
+++ components/Leaflet/index.tsx
@@ -34,34 +34,47 @@ export const Popup = dynamic(
 const MapBox: React.FC<WrapperProps & {
   currentCoords: CoordsState
 }> = ({ children, currentCoords }) => (
-  <Box
-    h="100%"
-    w={{
-      lg: '80%',
-      md: '100%',
-      sm: '100%',
-      base: '100%',
-    }}
-    borderRadius="20px"
-    overflow="clip"
-    css={{
-      '.leaflet-tile': {
-        filter: 'hue-rotate(180deg) invert(100%)',
-      },
-    }}
-  >
-    <MapContainer
-      /* @ts-ignore */
-      center={[currentCoords.lat as number, currentCoords.lng as number]}
-      zoomAnimation
-      zoom={15}
-      fadeAnimation
-      scrollWheelZoom
+  <>
+    <link
+      rel="stylesheet"
+      href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css"
+      integrity="sha512-hoalWLoI8r4UszCkZ5kL8vayOGVae1oxXe/2A4AO6J9+580uKHDO3JdHb7NzwwzK5xr/Fs0W40kiNHxM9vyTtQ=="
+      crossOrigin=""
+    />
+    <script
+      src="https://unpkg.com/leaflet@1.8.0/dist/leaflet.js"
+      integrity="sha512-BB3hKbKWOc9Ez/TAwyWxNXeoV9c1v6FIeYiBieIWkpLjauysF18NzgR1MBNBXf8/KABdlkX68nAhlwcDFLGPCQ=="
+      crossOrigin=""
+    />
+    <Box
+      h="100%"
+      w={{
+        lg: '80%',
+        md: '100%',
+        sm: '100%',
+        base: '100%',
+      }}
+      borderRadius="20px"
+      overflow="clip"
+      css={{
+        '.leaflet-tile': {
+          filter: 'hue-rotate(180deg) invert(100%)',
+        },
+      }}
     >
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      {children}
-    </MapContainer>
-  </Box>
+      <MapContainer
+        /* @ts-ignore */
+        center={[currentCoords.lat as number, currentCoords.lng as number]}
+        zoomAnimation
+        zoom={15}
+        fadeAnimation
+        scrollWheelZoom
+      >
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        {children}
+      </MapContainer>
+    </Box>
+  </>
 )
 
 export default MapBox
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,8 @@
 import Document, {
- Head,
- Html,
- Main,
- NextScript,
+  Head,
+  Html,
+  Main,
+  NextScript,
 } from 'next/document';
 
 class MyDocument extends Document {
@@ -29,17 +29,6 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Caveat:wght@700&display=swap"
             rel="stylesheet"
           />
-          <link
-            rel="stylesheet"
-            href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css"
-            integrity="sha512-hoalWLoI8r4UszCkZ5kL8vayOGVae1oxXe/2A4AO6J9+580uKHDO3JdHb7NzwwzK5xr/Fs0W40kiNHxM9vyTtQ=="
-            crossOrigin=""
-          />
-          <script
-            src="https://unpkg.com/leaflet@1.8.0/dist/leaflet.js"
-            integrity="sha512-BB3hKbKWOc9Ez/TAwyWxNXeoV9c1v6FIeYiBieIWkpLjauysF18NzgR1MBNBXf8/KABdlkX68nAhlwcDFLGPCQ=="
-            crossOrigin=""
-          />
           <meta name="theme-color" content="#000" />
           <meta name="description" content="Genesis Church App" />
 
@@ -47,13 +36,13 @@ class MyDocument extends Document {
           <meta name="description" content="Bem-vindo à Gênesis Church" />
 
           <meta property="og:type" content="website" />
-          <meta property="og:url" content="https://genesisproject-six.vercel.app" />
+          <meta property="og:url" content="https://www.genesischurch.app/" />
           <meta property="og:title" content="Genesis Church" />
           <meta property="og:description" content="Bem-vindos à Gênesis Church" />
           <meta property="og:image" content="/meta-bg.png" />
 
           <meta property="twitter:card" content="summary_large_image" />
-          <meta property="twitter:url" content="https://genesisproject-six.vercel.app" />
+          <meta property="twitter:url" content="https://www.genesischurch.app/" />
           <meta property="twitter:title" content="Genesis Church" />
           <meta property="twitter:description" content="Bem-vindos à Gênesis Church" />
           <meta property="twitter:image" content="/meta-bg.png" />
