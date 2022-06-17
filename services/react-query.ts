994e2bde0ef635380e3d76cba89a87b1ca2deddc
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2022-06-16 22:13:52 -0300

    chore: remove refetch on focus

--- services/react-query.ts
+++ services/react-query.ts
@@ -4,7 +4,6 @@ const ReactQueryClient = new QueryClient({
   defaultOptions: {
     queries: {
       refetchOnMount: false,
-      refetchOnWindowFocus: true,
       keepPreviousData: true,
       retryOnMount: false,
     },
