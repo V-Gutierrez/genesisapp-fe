2a12cd540717fa3b8b77c1be57610b999d0b8145
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2022-06-23 10:03:28 -0300

    chore: test image fallback implementation

--- components/NextImage/index.tsx
+++ components/NextImage/index.tsx
@@ -1,23 +1,32 @@
-import { Box, BoxProps as BoxPropsType } from '@chakra-ui/react';
+import { Box, BoxProps as BoxPropsType, Skeleton } from '@chakra-ui/react'
 import Image, { ImageProps as ImagePropsType } from 'next/image'
-
-import React from 'react';
+import React, { useState } from 'react'
 
 /**
-* @author: Victor Gutierrez
-* @description: Combination of Chakra Box and Next Image (https://nextjs.org/docs/api-reference/next/image)
-* */
+ * @author: Victor Gutierrez
+ * @description: Combination of Chakra Box and Next Image (https://nextjs.org/docs/api-reference/next/image)
+ * */
 
-const NextImage: React.FC<NextImageProps<ImagePropsType, BoxPropsType>> = ({ BoxProps, ImageProps }) => (
-    <Box
-      position="relative"
-      {...BoxProps}
-    >
-      <Image
-        {...ImageProps}
-        layout="fill"
-      />
+const NextImage: React.FC<NextImageProps<ImagePropsType, BoxPropsType>> = ({
+  BoxProps,
+  ImageProps,
+}) => {
+  const [loading, setLoading] = useState(false)
+
+  function handleLoadEnd() {
+    setLoading(false)
+  }
+
+  function handleLoadStart() {
+    setLoading(true)
+  }
+
+  return (
+    <Box position="relative" {...BoxProps}>
+      {loading && <Skeleton w="full" h="full" borderRadius="xl" />}
+      <Image {...ImageProps} layout="fill" onLoadStart={handleLoadStart} onLoad={handleLoadEnd} />
     </Box>
   )
+}
 
-export default NextImage;
+export default NextImage
