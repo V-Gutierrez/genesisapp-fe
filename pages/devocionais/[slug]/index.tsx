43c70153b9132d188a02a5ff58fdcec094ba4f8e
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2022-07-18 12:48:27 -0300

    feat: devotionals likes

--- pages/devocionais/[slug]/index.tsx
+++ pages/devocionais/[slug]/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import dynamic from 'next/dynamic'
 
+/* @ts-ignore */
 const DevotionalSection = dynamic(() => import('sections/Devotionals/DevotionalSection'))
 
 const DevotionalPage: NextPage = () => <DevotionalSection />
--- pages/noticias/[slug]/index.tsx
+++ pages/noticias/[slug]/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import dynamic from 'next/dynamic'
 
+/* @ts-ignore */
 const NewsSection = dynamic(() => import('sections/News/NewsSection'))
 
 const DevotionalPage: NextPage = () => <NewsSection />
