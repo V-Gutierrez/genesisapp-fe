ea5286349df408273beae03065d905745d956f1c
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2022-09-10 13:11:15 -0300

    fix: build

--- pages/devocionais/[slug]/index.tsx
+++ pages/devocionais/[slug]/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import dynamic from 'next/dynamic'
 
 /* @ts-ignore */
-const DevotionalSection = dynamic(() => import('sections/Devotionals/DevotionalSection'))
+const DevotionalSection = dynamic(() => import('sections/Client/Devotionals/DevotionalSection'))
 
 const DevotionalPage: NextPage = () => <DevotionalSection />
 
