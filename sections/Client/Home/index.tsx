5ad660b2f749a71c42e760d6b75225173f473723
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2023-05-01 18:13:54 -0300

    chore: changes

--- sections/Client/Home/index.tsx
+++ sections/Client/Home/index.tsx
@@ -1,10 +1,15 @@
 import DevotionalsSlider from 'sections/Client/Home/components/DevotionalsSlider'
-import { Flex } from '@chakra-ui/react'
+import { Flex, Text } from '@chakra-ui/react'
 import NewsHighlightsSlider from 'sections/Client/Home/components/NewsHighlightsSlider'
 
 export default function HomeSection() {
   return (
     <Flex flexDir="column" w="full" align="center">
+      <Flex pl={{ base: 2, md: 5 }} alignSelf="flex-start" flexDirection="column" w="100%" fontSize={{ base: '2xl', sm: '4xl' }} textAlign={{ base: 'left' }} mb={{base: 4, sm:2}}>
+        <Text fontFamily="Caveat" fontSize={{ base: '32px', sm: '45px' }}>Bem-vindos</Text>
+        <Text>à família Gênesis</Text>
+      </Flex>
+
       <NewsHighlightsSlider />
       <DevotionalsSlider />
     </Flex>
