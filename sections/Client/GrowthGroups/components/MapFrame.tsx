4fc7bc372b92a2c44857c1d4c316ea162bb92a9f
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2023-05-12 15:47:06 -0300

    chore: migrate Map

--- sections/Client/GrowthGroups/components/MapFrame.tsx
+++ sections/Client/GrowthGroups/components/MapFrame.tsx
@@ -1,53 +1,111 @@
-import { Skeleton } from '@chakra-ui/react'
-import MapBox, { Marker, Popup } from 'components/Leaflet'
-import React, { useEffect, useState } from 'react'
+import { Box, Skeleton } from '@chakra-ui/react'
+import {
+  DirectionsRenderer,
+  GoogleMap,
+  MarkerF,
+  useLoadScript,
+} from '@react-google-maps/api'
+import React, { useEffect, useMemo, useState } from 'react'
+import GCLogo from 'public/assets/images/gc-logo.png'
 
-import PopupContent from 'sections/Client/GrowthGroups/components/PopupContent'
-import { changeIcons } from 'sections/Client/GrowthGroups/components/utils'
-
-function MapFrame({ GCDataset, currentCoords, selectCoordsHandler }: MapFrameProps) {
-  const [userPosition, setUserPosition] = useState<CoordsState & { accurate: boolean }>({
+function MapFrame({
+  GCDataset,
+  currentCoords,
+  selectCoordsHandler,
+}: MapFrameProps) {
+  const [userPosition, setUserPosition] = useState<
+    CoordsState & { accurate: boolean }
+  >({
     lat: null,
     lng: null,
     accurate: false,
   })
-  const [mapLoading, setMapLoading] = useState(true)
 
-  function getUserLocation() {
+  const { isLoaded: mapIsLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY as string,
+    language: 'pt-BR',
+  })
+
+  const [directions, setDirections] = useState<google.maps.DirectionsResult>()
+  const [routeLoading, setRouteLoading] = useState<boolean>(false)
+
+  useEffect(() => {
+    // get current position from navigator
     navigator.geolocation.getCurrentPosition(
+      // if position is found, set current position and select coordinates
       (position) => {
         const { latitude, longitude } = position.coords
 
         setUserPosition({ lat: latitude, lng: longitude, accurate: true })
         selectCoordsHandler(latitude, longitude)
       },
+      // if position is not found, set default position and select coordinates
       () => {
-        setUserPosition({ lat: -34.6037, lng: -58.3816, accurate: false })
+        setUserPosition({ lat: -34.6037, lng: -58.3816, accurate: false }) // Buenos Aires - Obelisco
         selectCoordsHandler(-34.6037, -58.3816)
       },
+      // options
       {
         timeout: 30000,
         enableHighAccuracy: true,
       },
     )
+  }, [])
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const fetchRoutes = async () => {
+    setRouteLoading(true)
+    try {
+      const results = new google.maps.DirectionsService()
+
+      const config = {
+        destination: {
+          lat: currentCoords.lat as number,
+          lng: currentCoords.lng as number,
+        },
+        origin: {
+          lat: userPosition.lat as number,
+          lng: userPosition.lng as number,
+        },
+        travelMode: google.maps.TravelMode.TRANSIT,
+      }
+
+      const route = await results.route(config)
+
+      setDirections(route)
+    } catch (error) {
+      // console.warn(error)
+    }
+
+    setRouteLoading(false)
   }
 
   useEffect(() => {
-    getUserLocation()
-    changeIcons()
-  }, [])
+    if (mapIsLoaded && currentCoords.lat && currentCoords.lng) fetchRoutes()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentCoords.lat, currentCoords.lng])
 
-  useEffect(() => {
-    setMapLoading(true)
+  const mapOptions = useMemo<google.maps.MapOptions>(
+    () => ({
+      disableDefaultUI: false,
+      clickableIcons: false,
+      scrollwheel: true,
+      backgroundColor: '#edf2f7',
+    }),
+    [],
+  )
 
-    const delay = setTimeout(() => {
-      setMapLoading(false)
-      changeIcons()
-      clearTimeout(delay)
-    }, 250)
-  }, [currentCoords])
+  const shouldRenderLoading =
+    !mapIsLoaded ||
+    !GCDataset ||
+    !currentCoords.lat ||
+    !currentCoords.lng ||
+    routeLoading
 
-  if (mapLoading || !GCDataset || !currentCoords.lat || !currentCoords.lng) {
+  const shouldRenderMap =
+    mapIsLoaded && GCDataset && currentCoords.lat && currentCoords.lng
+
+  if (shouldRenderLoading) {
     return (
       <Skeleton
         h="100%"
@@ -61,34 +119,56 @@ function MapFrame({ GCDataset, currentCoords, selectCoordsHandler }: MapFramePro
     )
   }
 
-  if (GCDataset && currentCoords.lat && currentCoords.lng) {
+  if (shouldRenderMap) {
     return (
-      <MapBox currentCoords={currentCoords}>
-        {GCDataset.map((gc) => (
-          <Marker key={gc.id} position={[gc.lat, gc.lng]}>
-            <Popup
-              /* @ts-ignore */
-              className="customPopup"
-            >
-              <PopupContent {...gc} />
-            </Popup>
-          </Marker>
-        ))}
-
-        <Marker
-          position={[userPosition.lat as number, userPosition.lng as number]}
-          data-tip="userTip"
-          /* @ts-ignore */
-          title={userPosition.accurate ? 'Você está aqui' : 'Localização aproximada'}
+      <Box
+        h="100%"
+        w={{
+          lg: '80%',
+          md: '100%',
+          sm: '100%',
+          base: '100%',
+        }}
+        borderRadius="20px"
+        overflow="clip"
+      >
+        <GoogleMap
+          options={mapOptions}
+          zoom={10}
+          mapTypeId={google.maps.MapTypeId.HYBRID}
+          center={{
+            lat: currentCoords.lat as number,
+            lng: currentCoords.lng as number,
+          }}
+          mapContainerStyle={{ width: '100%', height: '100%' }}
         >
-          <Popup
-            /* @ts-ignore */
-            className="customPopup"
-          >
-            {userPosition.accurate ? 'Você está aqui' : 'Localização aproximada'}
-          </Popup>
-        </Marker>
-      </MapBox>
+          {GCDataset.map(({ lat, lng }) => (
+            <MarkerF
+              key={`${lat}-${lng}`}
+              position={{ lat, lng }}
+              icon={{
+                url: GCLogo.src,
+                scaledSize: new google.maps.Size(50, 60),
+              }}
+            />
+          ))}
+
+          <MarkerF
+            position={{
+              lat: userPosition.lat as number,
+              lng: userPosition.lng as number,
+            }}
+            title={
+              userPosition.accurate
+                ? 'Você está aqui'
+                : 'Localização aproximada'
+            }
+          />
+          {directions && (
+            <DirectionsRenderer options={{}} directions={directions} />
+          )}
+        </GoogleMap>
+      </Box>
     )
   }
   return null
