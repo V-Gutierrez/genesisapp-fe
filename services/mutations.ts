f6df6a815bb7849e824809257258001be4754a3d
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2023-05-20 11:23:05 -0300

    chore: update docs

--- services/mutations.ts
+++ services/mutations.ts
@@ -1,11 +1,19 @@
 import Axios from 'services/axios'
 
+/**
+ * Request password reset
+ * @param {ForgotPasswordFormValues} values - The email address of the user requesting the password reset
+ */
 export const REQUEST_RESET_PASSWORD = async (values: ForgotPasswordFormValues) => {
   await Axios.post<{ error: string }>('/auth/reset-password', {
     email: values.email,
   })
 }
 
+/**
+ * Login
+ * @param {LoginFormValues} param0 - The email and password of the user logging in
+ */
 export const LOGIN = async ({ email, password }: LoginFormValues) => {
   await Axios.post('/auth', {
     email,
@@ -13,6 +21,10 @@ export const LOGIN = async ({ email, password }: LoginFormValues) => {
   })
 }
 
+/**
+ * Sign up
+ * @param {SignUpFormValues} param0 - The user's email, password, name, phone number, birthdate, and region
+ */
 export const SIGN_UP = async ({
   email,
   password,
@@ -31,6 +43,10 @@ export const SIGN_UP = async ({
   })
 }
 
+/**
+ * Activate account
+ * @param {string} token - The activation token sent to the user's email
+ */
 export const ACTIVATE = async (token: string) => {
   await Axios.post(
     '/auth/activate',
@@ -43,6 +59,12 @@ export const ACTIVATE = async (token: string) => {
   )
 }
 
+/**
+ * Reset password
+ * @param {Object} param0 - The reset password token and the new password
+ * @param {string} param0.token - The reset password token sent to the user's email
+ * @param {string} param0.newPassword - The new password to set
+ */
 export const RESET_PASSWORD = async ({
   token,
   newPassword,
@@ -61,22 +83,36 @@ export const RESET_PASSWORD = async ({
   },
 )
 
+/**
+ * Logout
+ */
 export const LOGOUT = async () => {
   await Axios.delete('/auth/logout')
 }
 
+/**
+ * Create devotional
+ * @param {DevotionalFormValues} values - The details of the devotional to create
+ */
 export const CREATE_DEVOTIONAL = async (values: DevotionalFormValues) => {
   const formdata = new FormData()
 
+  // Append each key-value pair to the form data
   Object.entries(values).forEach(([key, value]) => {
     formdata.append(key, value)
   })
 
   await Axios.post('/admin/devotionals', formdata)
 }
+
+/**
+ * Create event
+ * @param {EventItemFormValues} values - The details of the event to create
+ */
 export const CREATE_EVENT = async (values: EventItemFormValues) => {
   const formdata = new FormData()
 
+  // Append each key-value pair to the form data
   Object.entries(values).forEach(([key, value]) => {
     formdata.append(key, value)
   })
@@ -84,24 +120,46 @@ export const CREATE_EVENT = async (values: EventItemFormValues) => {
   await Axios.post('/admin/events', formdata)
 }
 
+/**
+ * Delete devotional
+ * @param {string} id - The ID of the devotional to delete
+ */
 export const DELETE_DEVOTIONAL = async (id: string) => {
   await Axios.delete(`/admin/devotionals/${id}`)
 }
+
+/**
+ * Delete event
+ * @param {string} id - The ID of the event to delete
+ */
 export const DELETE_EVENT = async (id: string) => {
   await Axios.delete(`/admin/events/${id}`)
 }
 
+/**
+ * Delete event subscription
+ * @param {string} id - The ID of the event subscription to delete
+ */
 export const DELETE_EVENT_SUBSCRIPTION = async (id: string) => {
   await Axios.delete(`/admin/subscriptions/${id}`)
 }
 
+/**
+ * Like devotional
+ * @param {string} id - The ID of the devotional to like
+ */
 export const LIKE_DEVOTIONAL = async (id: string) => {
   await Axios.post(`/devotionals/${id}/like`)
 }
 
+/**
+ * Create news
+ * @param {NewsFormValues} values - The details of the news to create
+ */
 export const CREATE_NEWS = async (values: NewsFormValues) => {
   const formdata = new FormData()
 
+  // Append each key-value pair to the form data
   Object.entries(values).forEach(([key, value]) => {
     formdata.append(key, value)
   })
@@ -109,21 +167,44 @@ export const CREATE_NEWS = async (values: NewsFormValues) => {
   await Axios.post('/admin/news', formdata)
 }
 
+/**
+ * Create growth group
+ * @param {GrowthGroupFormValues} values - The details of the growth group to create
+ */
 export const CREATE_GROWTH_GROUP = async (values: GrowthGroupFormValues) => {
   await Axios.post('/admin/growthgroups', values)
 }
 
+/**
+ * Delete news
+ * @param {string} id - The ID of the news to delete
+ */
 export const DELETE_NEWS = async (id: string) => {
   await Axios.delete(`/admin/news/${id}`)
 }
+
+/**
+ * Delete growth group
+ * @param {string} id - The ID of the growth group to delete
+ */
 export const DELETE_GROWTH_GROUP = async (id: string) => {
   await Axios.delete(`/admin/growthgroups/${id}`)
 }
 
+/**
+ * Like news
+ * @param {string} id - The ID of the news to like
+ */
 export const LIKE_NEWS = async (id: string) => {
   await Axios.post(`/news/${id}/like`)
 }
 
+/**
+ * Subscribe to event
+ * @param {Object} param0 - The ID of the event to subscribe to and the details of the subscription
+ * @param {string} param0.id - The ID of the event to subscribe to
+ * @param {Partial<EventSubscriptionFormValues>} param0.values - The details of the subscription
+ */
 export const SUBSCRIBE_TO_EVENT = async ({
   id,
   values,
@@ -131,6 +212,7 @@ export const SUBSCRIBE_TO_EVENT = async ({
   id: string
   values: Partial<EventSubscriptionFormValues>
 }) => {
+  // Remove the "region" key from the values object
   delete values.region
 
   await Axios.post(`/events/${id}/subscriptions`, values)
--- services/queries.ts
+++ services/queries.ts
@@ -1,9 +1,38 @@
 import Axios from 'services/axios'
 
+/**
+ * Functions related to user data
+ */
+
+/**
+ * Get the current user's data
+ * @returns {Promise<Partial<User>>} - Promise that resolves to the user's data
+ */
 export const GET_ME = async () => Axios.get<Partial<User>>('/auth/me')
+
+/**
+ * Get the growth groups
+ * @returns {Promise<GrowthGroup[]>} - Promise that resolves to an array of growth groups
+ */
 export const GET_GROWTH_GROUPS = async () => Axios.get<GrowthGroup[]>('/growthGroups')
+
+/**
+ * Get the devotionals for admins
+ * @returns {Promise<Devotional[]>} - Promise that resolves to an array of devotionals
+ */
 export const GET_ADMIN_DEVOTIONALS = async () => Axios.get<Devotional[]>('/admin/devotionals')
+
+/**
+ * Get the devotionals for a user
+ * @returns {Promise<Devotional[]>} - Promise that resolves to an array of devotionals
+ */
 export const GET_USER_DEVOTIONALS = async () => Axios.get<Devotional[]>('/devotionals')
+
+/**
+ * Get a devotional by its slug
+ * @param {RQParams} ReactQueryParams - React Query parameters
+ * @returns {Promise<Devotional>} - Promise that resolves to the devotional
+ */
 export const GET_DEVOTIONAL_BY_SLUG = async (ReactQueryParams: RQParams) => {
   const { 1: devotionalSlug } = ReactQueryParams.queryKey as string[]
 
@@ -12,12 +41,29 @@ export const GET_DEVOTIONAL_BY_SLUG = async (ReactQueryParams: RQParams) => {
   }
 }
 
+/**
+ * Get the app's statistics
+ * @returns {Promise<Stats>} - Promise that resolves to the app's statistics
+ */
 export const GET_STATS = async () => Axios.get<Stats>('/stats')
 
+/**
+ * Refresh the user's token
+ * @returns {Promise<any>} - Promise that resolves to the refreshed token
+ */
 export const REFRESH_TOKEN = async () => Axios.get('/auth')
 
+/**
+ * Get the app's users (for admins)
+ * @returns {Promise<User[]>} - Promise that resolves to an array of users
+ */
 export const GET_USERS = async () => Axios.get<User[]>('/admin/users')
 
+/**
+ * Get the photos from a Google Photos album
+ * @param {RQParams} ReactQueryParams - React Query parameters
+ * @returns {Promise<GooglePhotosImageSet[]>} - Promise that resolves to an array of photos
+ */
 export const GET_GOOGLE_PHOTOS_ALBUM_PHOTOS = async (ReactQueryParams: RQParams) => {
   const { 1: albumUrl } = ReactQueryParams.queryKey as string[]
 
@@ -26,8 +72,27 @@ export const GET_GOOGLE_PHOTOS_ALBUM_PHOTOS = async (ReactQueryParams: RQParams)
   }
 }
 
+/**
+ * Functions related to news data
+ */
+
+/**
+ * Get the news for admins
+ * @returns {Promise<News[]>} - Promise that resolves to an array of news
+ */
 export const GET_ADMIN_NEWS = async () => Axios.get<News[]>('/admin/news')
+
+/**
+ * Get the news for a user
+ * @returns {Promise<News[]>} - Promise that resolves to an array of news
+ */
 export const GET_USER_NEWS = async () => Axios.get<News[]>('/news')
+
+/**
+ * Get a news item by its slug
+ * @param {RQParams} ReactQueryParams - React Query parameters
+ * @returns {Promise<News>} - Promise that resolves to the news item
+ */
 export const GET_NEWS_BY_SLUG = async (ReactQueryParams: RQParams) => {
   const { 1: newsSlug } = ReactQueryParams.queryKey as string[]
 
@@ -36,7 +101,21 @@ export const GET_NEWS_BY_SLUG = async (ReactQueryParams: RQParams) => {
   }
 }
 
+/**
+ * Functions related to event data
+ */
+
+/**
+ * Get the events for a user
+ * @returns {Promise<EventItem[]>} - Promise that resolves to an array of events
+ */
 export const GET_USER_EVENTS = async () => Axios.get<EventItem[]>('/events')
+
+/**
+ * Get an event by its ID
+ * @param {RQParams} ReactQueryParams - React Query parameters
+ * @returns {Promise<EventItem>} - Promise that resolves to the event
+ */
 export const GET_EVENTS_BY_ID = async (ReactQueryParams: RQParams) => {
   const { 1: eventId } = ReactQueryParams.queryKey as string[]
 
@@ -45,6 +124,18 @@ export const GET_EVENTS_BY_ID = async (ReactQueryParams: RQParams) => {
   }
 }
 
+/**
+ * Get the events for admins
+ * @returns {Promise<EventItem[]>} - Promise that resolves to an array of events
+ */
 export const GET_ADMIN_EVENTS = async () => Axios.get<EventItem[]>('/admin/events')
 
+/**
+ * Functions related to region data
+ */
+
+/**
+ * Get the regions
+ * @returns {Promise<RegionCollectionItem[]>} - Promise that resolves to an array of regions
+ */
 export const GET_REGIONS = async () => Axios.get<RegionCollectionItem[]>('/regions')
