7cc93bdf53a3e7adf2bffe007b632119f5806780
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2023-05-26 10:46:20 -0300

    chore: revert oneSignal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,33 +9,8 @@ import { ReactQueryDevtools } from 'react-query/devtools'
 import SidebarWithHeader from 'components/Drawer'
 import { UserContextProvider } from 'context/UserContext'
 import { chakraTheme } from 'styles/chakraTheme'
-import { useEffect } from 'react'
 
-declare global {
-  interface Window {
-    OneSignal: any;
-  }
-}
 function MyApp({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    if (typeof window === 'undefined') return;
-
-      window.OneSignal = window.OneSignal || [];
-      window.OneSignal.push(() => {
-        window.OneSignal.init({
-          appId: 'b40b7cc7-13dc-4662-8b48-efa668f9b72a',
-          notifyButton: {
-            enable: true,
-          },
-          allowLocalhostAsSecureOrigin: true,
-        });
-      });
-
-    return () => {
-      window.OneSignal = undefined;
-    };
-  }, []); // <-- run this effect once on mount
-
   return (
     <>
       <Head>
