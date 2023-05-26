7237182ae8bdad9e3061ff3c6d4196f9ca350ca2
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2023-05-26 13:34:03 -0300

    chore: add safari web id

--- components/ReactOneSignal/index.tsx
+++ components/ReactOneSignal/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 class ReactOneSignal extends React.Component {
   oneSignal = () => ({
-    __html: `window.OneSignal = window.OneSignal || []; OneSignal.push(function() { OneSignal.init({ appId: "${process.env.NEXT_PUBLIC_ONESIGNAL_APP_ID}", safari_web_id: "${process.env.NEXT_PUBLIC_SAFARIWEBID}"});});`,
+    __html: `window.OneSignal = window.OneSignal || []; OneSignal.push(function() { OneSignal.init({ appId: "${process.env.NEXT_PUBLIC_ONESIGNAL_APP_ID}", safari_web_id: "${process.env.NEXT_PUBLIC_SAFARIWEBID}", welcomeNotification: { disable: false }, promptOptions: { slidedown: { enabled: true, actionMessage: "Gostaria de receber notificações para as últimas novidades e atualizações?", acceptButtonText: "SIM", cancelButtonText: "NÃO" } } });});`,
   })
 
   render() {
